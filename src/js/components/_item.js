2bc8da2ea8f00912325ed4d3972b48d84829b10e
whats0n <mrdvv@mail.ru>
2018-02-23 16:57:01 +0200

    start main

--- src/js/components/_item.js
+++ src/js/components/_item.js
@@ -1,6 +1,50 @@
-import {getIcon} from '../_utils';
+import {tabletWidthStart, tabletWidthEnd, WIN, ACTIVE, OPEN} from '../_constants';
+import {getIcon, getWidth} from '../_utils';
 
 export default (() => {
+
+  const owlDefault = 'owl-carousel';
+  const items = $('.js-items');
+  const sliders = $('.js-items-slider');
+
+  WIN.on('resize load', () => {
+    if (getWidth(tabletWidthEnd) && !sliders.hasClass('owl-loaded')) {
+      sliders
+        .addClass(owlDefault)
+        .owlCarousel({
+          items: 1,
+          center: true,
+          stagePadding: 22,
+          loop: true,
+          nav: false,
+          dots: false,
+          responsive: {
+            [tabletWidthStart]: {
+              items: 2,
+              stagePadding: 5,
+              center: false
+            }
+          }
+        });
+    } else if (!getWidth(tabletWidthEnd) && sliders.hasClass('owl-loaded')) {
+      sliders
+        .removeClass(owlDefault)
+        .trigger('destroy.owl.carousel');
+    }
+  });
+
+  items.each((i, item) => {
+    item = $(item);
+    const slider = item.find('.js-items-slider');
+    const toggle = item.find('.js-items-toggle');
+    const content = item.find('.js-items-content');
+
+    toggle.on('click', e => {
+      e.preventDefault();
+      toggle.toggleClass(ACTIVE);
+      content.toggleClass(OPEN);
+    });
+  });
 
   $('.js-item-slider').owlCarousel({
     items: 1,
